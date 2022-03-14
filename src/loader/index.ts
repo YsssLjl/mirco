7a7a6f8195ec56edeb5e155638735490ca4cfd49
wxy <237510694@qq.com>
2022-03-14 12:11:41 +0800

    feat comment

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,45 +1,58 @@
-import { InternalAppInfo } from "../types";
-import { importEntry } from "import-html-entry";
-import { ProxySandbox } from "./sandbox";
+/*
+ * @Author: OBKoro1
+ * @Date: 2022-03-14 11:54:49
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2022-03-14 12:05:30
+ * @FilePath: \micro_test\src\loader\index.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by 用户/公司名, All Rights Reserved. 
+ */
+import { InternalAppInfo } from '../types'
+import { importEntry } from 'import-html-entry'
+import { ProxySandbox } from './sandbox'
 
 export const loadHTML = async (app: InternalAppInfo) => {
-  const { container, entry } = app;
+  const { container, entry } = app
 
   const { template, getExternalScripts, getExternalStyleSheets } =
-    await importEntry(entry);
-  const dom = document.querySelector(container);
+    await importEntry(entry)
+  const dom = document.querySelector(container)
 
   if (!dom) {
-    throw new Error("容器不存在");
+    throw new Error('容器不存在')
   }
 
-  dom.innerHTML = template;
-  await getExternalStyleSheets();
-  const jsCode = await getExternalScripts();
-  jsCode.forEach((script) => {
-    const lifeCycle = runJS(script, app);
+  dom.innerHTML = template
+  await getExternalStyleSheets()
+  const jsCode = await getExternalScripts()
+  jsCode.forEach(script => {
+    const lifeCycle = runJS(script, app)
     if (lifeCycle) {
-      app.bootstrap = lifeCycle.bootstrap;
-      app.mount = lifeCycle.mount;
-      app.unmount = lifeCycle.unmount;
+      app.bootstrap = lifeCycle.bootstrap
+      app.mount = lifeCycle.mount
+      app.unmount = lifeCycle.unmount
     }
-  });
+  })
 
-  return app;
-};
+  return app
+}
 
 const runJS = (value: string, app: InternalAppInfo) => {
   if (!app.proxy) {
-    app.proxy = new ProxySandbox();
+    app.proxy = new ProxySandbox()
+    // 将沙箱挂在全局属性上
     // @ts-ignore
-    window.__CURRENT_PROXY__ = app.proxy.proxy;
+    window.__CURRENT_PROXY__ = app.proxy.proxy
   }
-  app.proxy.active();
+  // 激活沙箱
+  app.proxy.active()
+  // 用沙箱替代全局环境调用 JS
   const code = `
     return (window => {
       ${value}
       return window['${app.name}']
     })(window.__CURRENT_PROXY__)
-  `;
-  return new Function(code)();
-};
+  `
+  return new Function(code)()
+}
--- src/loader/sandbox.ts
+++ src/loader/sandbox.ts
@@ -1,43 +1,49 @@
 export class ProxySandbox {
-  proxy: any;
-  running = false;
+  proxy: any
+  running = false
   constructor() {
-    const fakeWindow = Object.create(null);
+    // *创建个假的 window
+    const fakeWindow = Object.create(null)
     const proxy = new Proxy(fakeWindow, {
       set: (target: any, p: string, value: any) => {
+        // *如果当前沙箱在运行，就直接把值设置到 fakeWindow 上
         if (this.running) {
-          target[p] = value;
+          target[p] = value
         }
-        return true;
+        return true
       },
       get(target: any, p: string): any {
         switch (p) {
-          case "window":
-          case "self":
-          case "globalThis":
-            return proxy;
+          case 'window':
+          case 'self':
+          case 'globalThis':
+            return proxy
         }
+        // *假如属性不存在 fakeWindow 上，但是存在于 window 上
+        // *从 window 上取值
         if (
           !window.hasOwnProperty.call(target, p) &&
           window.hasOwnProperty(p)
         ) {
           // @ts-ignore
-          const value = window[p];
-          if (typeof value === "function") return value.bind(window);
-          return value;
+          const value = window[p]
+          if (typeof value === 'function') return value.bind(window)
+          return value
         }
-        return target[p];
+        return target[p]
       },
       has() {
-        return true;
-      },
-    });
-    this.proxy = proxy;
+        return true
+      }
+    })
+    this.proxy = proxy
   }
+  // *激活沙箱
   active() {
-    this.running = true;
+    this.running = true
   }
+  // *失活
   inactive() {
-    this.running = false;
+    this.running = false
   }
 }
